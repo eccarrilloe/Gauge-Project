efd0ae857fc6ac26859b8b3f8b7e65b939bc6c0e
Edward Carrillo <edwardc@gmlsoftware.com>
2020-07-16 08:38:11 -0500

    feat(web): first version of web component added

---
+++ web/src/app/clients/sockets/gauges/gauges.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GaugesRoutingModule } from './gauges-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    GaugesRoutingModule
+  ]
+})
+export class GaugesModule { }
